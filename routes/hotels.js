0340a1aaeba8a871b575ff23934fc67c58b0b91a
mirazhasan47 <hasan3754@diu.edu.bd>
2023-03-24 16:29:51 +0600

    update

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,21 +1,12 @@
 import express from "express"
+import { createHotel } from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js"
+import createError from "../utils/error.js"
 
 const router = express.Router();
 
 //CREATE
-router.post("/", async(req, res) => {
-
-    const newHotel = new Hotel(req.body)
-
-    try {
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
-    } catch (err) {
-        res.status(500).json(err)
-    }
-    
-})
+router.post("/", createHotel)
 
 //UPDATE
 
@@ -54,8 +45,11 @@ router.get("/:id", async(req, res) => {
 //get all
 
 router.get("/", async(req, res, next) => {
+
+    const failed = true;
+
     try {
-        const hotels = await Hotel.findById("sdasd")
+        const hotels = await Hotel.find()
         res.status(200).json(hotels)
     } catch (err) {
         next(err)
