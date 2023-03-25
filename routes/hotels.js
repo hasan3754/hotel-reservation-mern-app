5bb3d2c386559f7b09223b3e7395223bc578db9d
mirazhasan47 <hasan3754@diu.edu.bd>
2023-03-25 16:19:06 +0600

    up

--- routes/hotels.js
+++ routes/hotels.js
@@ -2,18 +2,19 @@ import express from "express"
 import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js"
 import createError from "../utils/error.js"
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 //CREATE
-router.post("/", createHotel)
+router.post("/", verifyAdmin, createHotel)
 
 //UPDATE
 
-router.put("/:id", updateHotel)
+router.put("/:id",verifyAdmin, updateHotel)
 
 //delete
-router.delete("/:id", deleteHotel)
+router.delete("/:id",verifyAdmin, deleteHotel)
 
 //get
 router.get("/:id", getHotel)
